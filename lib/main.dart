86cc08b8135be6b2385eae7df0522a092e9d3199
Ibrahim Hendi Zulkarnaen Nasution <ibra.nasution0307@gmail.com>
2024-05-21 16:40:19 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: PopularFoodDetail(),
+      home: food_page_body(),
     );
   }
 }
